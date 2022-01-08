2d0bfaec25b2eee8ef348fb0dbf14aedbbbbb7cb
ebelgica <ebelgica@johnsonsberkshire.com>
2022-01-08 11:32:29 +0800

    created files table

---
+++ database/migrations/2022_01_08_033011_create_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('filename');
+            $table->string('title');
+            $table->string('description');
+            $table->integer('programming_language_id');
+            $table->tinyInteger('is_module');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
