9e0d257fd4845e071e7277f1e77e77f69a646d14
PaulSong213 <songaliajohnpaul@gmail.com>
2022-01-10 09:41:05 +0800

    Add search on home and file page

--- resources/views/files/index.blade.php
+++ resources/views/files/index.blade.php
@@ -10,12 +10,38 @@
     <h1 class="text-center fw-bold fs-1 my-4">
       {{$progLang->name}}
     </h1>
+
+    <div class="container mb-3 d-flex justify-content-between px-5">
+        <form action="/file" method="get">
+        <div class="input-group" style="max-width: 300px;">
+            <input type="hidden" name="progLangId" value="{{$_GET['progLangId']}}">
+            @if(isset($_GET['query']) && $_GET['query'] )
+                <input type="search" name="query" class="form-control rounded" placeholder="Search" value="{{$_GET['query']}}" />
+            @else
+                <input type="search" name="query" class="form-control rounded" placeholder="Search" />
+            @endif
+            <button type="submit" class="btn btn-outline-primary">search</button>
+        </div>
+        </form>
+        @if(isset($_GET['query']) && $_GET['query'] )
+        <div class="d-flex flex-column">
+            <a class="fs-6" style="text-align: right;" href="/programming-languages">Show All</a>
+            <h6 class="text-success fw-bold fs-5">Search Result for: {{$_GET['query']}} </h6>
+        </div>
+        @endif
+    </div>
+
     
 
     <div class="container w-100">
       <div class="">
-
+      @if(sizeof($files) <= 0 )
+      <h6 class="text-center text-danger fw-bold">No Data Found</h6>
+      @endif
       @foreach ($files as $file)
+        @if(isset($_GET['query']) && $_GET['query'] && $file->programming_language_id != $_GET['progLangId'] )
+          @continue
+        @endif
         <div class="mx-auto p-3 w-100" style="max-width: 800px;">
           <a class="text-decoration-none d-flex flex-column pt-3 rounded shadow-sm bg-light" href="/file/{{$file->id}}" style="height: 130px;">
             <div class="px-3 row ">
--- resources/views/programmingLanguages/index.blade.php
+++ resources/views/programmingLanguages/index.blade.php
@@ -5,9 +5,30 @@
 
     <h1 class="text-center fw-bold fs-1 mb-5">Programming Languages</h1>
 
+    <div class="container mb-3 d-flex justify-content-between">
+        <form action="/programming-languages" method="get">
+        <div class="input-group" style="max-width: 300px;">
+            @if(isset($_GET['query']) && $_GET['query'] )
+                <input type="search" name="query" class="form-control rounded" placeholder="Search" value="{{$_GET['query']}}" />
+            @else
+                <input type="search" name="query" class="form-control rounded" placeholder="Search" />
+            @endif
+            <button type="submit" class="btn btn-outline-primary">search</button>
+        </div>
+        </form>
+        @if(isset($_GET['query']) && $_GET['query'] )
+        <div class="d-flex flex-column">
+            <a class="fs-6" style="text-align: right;" href="/programming-languages">Show All</a>
+            <h6 class="text-success fw-bold fs-5">Search Result for: {{$_GET['query']}} </h6>
+        </div>
+        @endif
+    </div>
+
     <div class="container w-100">
       <div class="row ">
-
+      @if(sizeof($progLang) <= 0 )
+      <h6 class="text-center text-danger fw-bold">No Data Found</h6>
+      @endif
       @foreach ($progLang as $lang)
         <div class="col-12 col-md-6 col-xl-4 p-3">
           <a class="text-decoration-none d-flex flex-column justify-content-center rounded shadow bg-light" href="file?progLangId={{$lang->id}}"  style="height: 180px;">
